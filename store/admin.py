e95886dfa4809b3548e82ca4c21b23662db815e6
Saeed812 <134283764+Saeed812@users.noreply.github.com>
2024-04-14 10:14:42 -0700

    comit test

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import category, customer, product, order
 
+admin.site.register(category)
+admin.site.register(customer)
+admin.site.register(product)
+admin.site.register(order)
 
